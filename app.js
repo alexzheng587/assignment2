044e541a061861daa9e9aaac718677f549765935
alex587 <alex587@students.cs.ubc.ca>
2020-07-20 00:01:25 -0700

    static build add

--- app.js
+++ app.js
@@ -24,12 +24,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('client/build'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', messageRouter);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
